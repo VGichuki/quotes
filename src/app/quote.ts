4f24f062b869225982c9b4efc94311449efd0668
VGichuki <vivian.gichuki@student.moringaschool.com>
2021-05-18 10:42:11 +0300

    make changes to quote.ts

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -3,7 +3,7 @@ export class Quote {
     upVote: number;
     downVote: number;
 
-    constructor(public author: string, public quote: string, public per: string, public date: Date){
+    constructor(public author: string, public quote: string, public per: string, public completeDate:Date){
         this.showDetails = false;
         this.upVote = 0;
         this.downVote =0;
