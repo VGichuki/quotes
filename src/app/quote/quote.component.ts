50b4cb95fc706e01d1688ed9b32075bfee037d7e
VGichuki <vivian.gichuki@student.moringaschool.com>
2021-05-18 10:41:38 +0300

    make changes to quote component

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -10,8 +10,24 @@ export class QuoteComponent implements OnInit {
 
   quotes:Quote[] = [
       new Quote('Elon Musk', 'When something is important enough, you do it even if the odds are not in your favor.', 'Vivian' ,new Date()),
-      new Quote('Elon Musk', "Patience is a virtue, and I'm learning patience.It's a tough lesson.", 'Vivian', new Date())
+      new Quote('Elon Musk', "Patience is a virtue, and I'm learning patience.It's a tough lesson.", 'Wangari', new Date())
   ];
+  addNewQuote(quote: any){
+    let quoteLength = this.quotes.length;
+    quote.id = quoteLength+1;
+    quote.completeDate = new Date(quote.completeDate)
+    this.quotes.push(quote)
+  }
+
+  deleteGoal(isComplete: any, index: number){
+    if (isComplete) {
+      let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].author}?`)
+
+      if (toDelete){
+        this.quotes.splice(index,1)
+      }
+    }
+  }
   toggleDetails(index:number){
     this.quotes[index].showDetails = !this.quotes[index].showDetails;
   }
