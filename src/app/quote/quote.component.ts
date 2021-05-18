67fc057ea70c1b929c98dad932e7ab216109df48
VGichuki <vivian.gichuki@student.moringaschool.com>
2021-05-18 11:38:12 +0300

    add background image to the website

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,8 +9,8 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes:Quote[] = [
-      new Quote('Elon Musk', 'When something is important enough, you do it even if the odds are not in your favor.', 'Vivian' ,new Date()),
-      new Quote('Elon Musk', "Patience is a virtue, and I'm learning patience.It's a tough lesson.", 'Wangari', new Date())
+      new Quote('Elon Musk', 'When something is important enough, you do it even if the odds are not in your favor.', 'Vivian' ,new Date(2018,4,18)),
+      new Quote('Elon Musk', "Patience is a virtue, and I'm learning patience.It's a tough lesson.", 'Wangari', new Date(2020,9,2))
   ];
   addNewQuote(quote: any){
     let quoteLength = this.quotes.length;
